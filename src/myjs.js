4e561f2f1677600a0d74513ff0522e4aa6ac7e13
Inderjit S. Sood <sirs025@gmail.com>
2020-10-11 12:43:41 +0530

    First stable commit

---
+++ src/myjs.js
@@ -0,0 +1,6 @@
+
+
+function myParagraphFunction() {
+	const myText = (<h1> Fuck oh yeah! </h1>); //This is a component
+	ReactDOM.render(myText, document.getElementById('a_div'));   //This renders the component to DOM
+}
